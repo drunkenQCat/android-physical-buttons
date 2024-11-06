7b4942691cdc051236a38ce38049fca1a25c2751
Quantum Cat <songjh123123@outlook.com>
2024-11-07 01:32:20 +0800

    migrate to API Level 35

--- example/android/app/src/main/kotlin/cn/wingsj/android_physical_buttons_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/cn/wingsj/android_physical_buttons_example/MainActivity.kt
@@ -2,5 +2,4 @@ package cn.wingsj.android_physical_buttons_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
